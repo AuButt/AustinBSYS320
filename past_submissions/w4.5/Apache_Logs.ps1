c5d5430f14dc70b5051003e2574a46cba839f92f
Austin <austin.butt@mymail.champlain.edu>
2026-01-08 15:34:52 -0500

    test

---
+++ past_submissions/w4.5/Apache_Logs.ps1
@@ -0,0 +1,37 @@
+﻿function Get-Logs{
+    param([string]$ref,
+          [string]$http,
+           [string]$browser)
+
+    $path = "C:\xampp\apache\logs\access.log"
+
+    Get-Content $path |
+    Select-String $ref |
+    Select-String $http |
+    Select-String $browser |
+    Group-Object |
+    Select-Object Name, Count
+
+}
+
+#Parsing Apache Logs
+function ApacheLogs1(){
+$logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i = 0; $i -lt $logsNotformatted.Count; $i++) {
+$words = $logsNotformatted[$i].Split(" ");
+
+$tableRecords += [pscustomobject]@{
+"IP" = $words[0]
+"Time" = $words[3].Trim('[')
+"Method" = $words[5].Trim("")
+"Page" = $words[6];
+"Protocol" = $words[7].Trim("")
+"Response" = $words[8]
+"Referrer" = $words[10]
+"Client" = $words[11..($words.ounct - 1)];
+}
+}
+return $tableRecords | Where-Object {$_.IP -like "10.*"}
+}
---
+++ past_submissions/w4.5/main.ps1
@@ -0,0 +1,8 @@
+﻿# from assignment week4
+#..\Apache_Logs.ps1
+#Get-Logs "index.html" "404" "Chrome"
+
+#parsing apache logs
+.\Apache_Logs.ps1
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
